abfa2145ca3983fefd64bb473734f707c57e128c
袁孝柏 <yuanxiaobai_java@163.com>
2018-09-06 16:15:18 +0800

    again

--- text/src/com/xb/text/Test.java
+++ text/src/com/xb/text/Test.java
@@ -3,6 +3,6 @@ package com.xb.text;
 public class Test {
 	public static void main(String[] args) {
 		System.out.println("hugedata.com.cn");
-		System.out.println("xiaobai update Test.java");
+		System.out.println("xiaobai update Test.java again");
 	}
 }
