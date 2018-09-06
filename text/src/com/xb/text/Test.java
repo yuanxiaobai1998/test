504b6bbf5aff13d2d56af96de008fc3b13cfeb11
袁孝柏 <yuanxiaobai_java@163.com>
2018-09-06 16:09:50 +0800

    update

--- text/src/com/xb/text/Test.java
+++ text/src/com/xb/text/Test.java
@@ -3,6 +3,6 @@ package com.xb.text;
 public class Test {
 	public static void main(String[] args) {
 		System.out.println("hugedata.com.cn");
-		System.out.println("xiaobai修改了Test.java文件");
+		System.out.println("xiaobai update Test.java");
 	}
 }
